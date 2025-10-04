c3412b0f8afc03605a9f30e19f6e377968ae04f0
alireza139 <alireza.hosseinii66@gmail.com>
2025-10-04 15:18:12 +0330

    Tenth commit

--- src/pages/singleProduct/SingleProduct.js
+++ src/pages/singleProduct/SingleProduct.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import productList from '../../productsDATA'
-import { useParams , Navigate } from 'react-router-dom'
+import { useParams, Navigate } from 'react-router-dom'
 import { Image } from 'react-bootstrap'
 import './SingleProduct.css'
+import '../../custom.css'
+import { Container } from 'react-bootstrap'
 
 
 export default function SingleProduct(props) {
@@ -16,27 +18,30 @@ export default function SingleProduct(props) {
 
     return (
         hasProduct ? (
-            <div className='single-page w-100 d-flex justify-content-evenly align-items-center'>
-                <div className='single-page-info'>
-                    <h4 className='mb-3'>
-                        <span className='fw-bold'>Product name:</span>
-                        {` ${chosenProduct.name}`}
-                    </h4>
-                    <p>
-                        <span className='fw-bold'>product ID:</span>
-                        {` 00${chosenProduct.id}`}
-                    </p>
-                    <p>
-                        <span className='fw-bold'>Price:</span>
-                        {` ${chosenProduct.price}$`}
-                    </p>
-                    <p>
-                        <span className='fw-bold'>Details:</span>
-                        <br />
-                        {productDetails}
-                    </p>
-                </div>
-                <Image src={`/${chosenProduct.img}`} className='single-page-img m-0 p-0' />
+            <div className='bg-light'>
+                <Container className='single-page pt-5 pb-3'>
+                    <Image src={`/${chosenProduct.img}`} className='single-page-img single-page-img-sm  m-0 p-0 mb-3' />
+                    <div className='single-page-info'>
+                        <h4 className='mb-3'>
+                            <span className='fw-bold'>Product name:</span>
+                            {` ${chosenProduct.name}`}
+                        </h4>
+                        <p>
+                            <span className='fw-bold'>product ID:</span>
+                            {` 00${chosenProduct.id}`}
+                        </p>
+                        <p>
+                            <span className='fw-bold'>Price:</span>
+                            {` ${chosenProduct.price}$`}
+                        </p>
+                        <p>
+                            <span className='fw-bold'>Details:</span>
+                            <br />
+                            {productDetails}
+                        </p>
+                    </div>
+                    <Image src={`/${chosenProduct.img}`} className='single-page-img single-page-img-lg m-0 p-0' />
+                </Container>
             </div>
         ) : (
             <Navigate to="/ERROR404"></Navigate>
